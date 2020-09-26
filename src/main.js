16edc07e7c0170ee620332b07806f5fb65c729d3
java0088 <965002107@qq.com>
2020-09-26 14:16:42 +0800

    sun-ui移动端样式库初始化

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 引入sun-ui
+import Sun from 'vue-sun-ui'
+// 引入sun-ui样式文件
+import 'vue-sun-ui/dist/vue-sun-ui.css'
 Vue.config.productionTip = false
-
+Vue.use(Sun)
 new Vue({
   router,
   render: h => h(App)
