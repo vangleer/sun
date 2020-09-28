1db826f7c4846385b0f8a8faac5e1c0182ef77d3
hong <1060850932@qq.com>
2020-09-28 15:14:08 +0800

    Toast组件展示完成

--- src/views/Popup.vue
+++ src/views/Popup.vue
@@ -1,10 +1,121 @@
 <template>
-  <div class="sun-demo-popup"></div>
+  <div class="sun-demo-popup">
+    <sun-nav-bar title="Popup" left-arrow @left-click="$router.back()" />
+
+    <!-- 基础用法 -->
+    <div class="sun-demo-block">
+      <!-- <sun-cell is-link @click="showPopup">展示弹出层</sun-cell>\n<sun-popup v-model="show">内容</sun-popup> -->
+      <h2 class="sun-demo-nav-title">基础用法</h2>
+      <div class="sun-demo-nav-row">
+        <sun-cell title="弹出层展示" is-link bg-cffect @click="handleShow('center')" />
+      </div>
+    </div>
+    <!-- 弹出位置 -->
+    <div class="sun-demo-block">
+      <!-- <sun-cell is-link @click="showPopup">展示弹出层</sun-cell>\n<sun-popup v-model="show">内容</sun-popup> -->
+      <h2 class="sun-demo-nav-title">弹出位置</h2>
+      <div class="sun-demo-nav-row">
+        <sun-cell title="顶部弹出" is-link bg-cffect @click="handleShow('top')" />
+        <sun-cell title="底部弹出" is-link bg-cffect @click="handleShow('bottom')" />
+        <sun-cell title="左侧弹出" is-link bg-cffect @click="handleShow('left')" />
+        <sun-cell title="右侧弹出" is-link bg-cffect @click="handleShow('right')" />
+      </div>
+    </div>
+
+    <!-- 关闭图标 -->
+    <div class="sun-demo-block">
+      <h2 class="sun-demo-nav-title">关闭图标</h2>
+      <div class="sun-demo-nav-row">
+        <sun-cell title="关闭图标" is-link bg-cffect @click="showClose1=true" />
+        <sun-cell title="自定义图标" is-link bg-cffect @click="showClose2=true" />
+        <sun-cell title="图标位置" is-link bg-cffect @click="showClose3=true" />
+      </div>
+    </div>
+
+    <!-- 圆角弹窗 -->
+    <div class="sun-demo-block">
+      <h2 class="sun-demo-nav-title">圆角弹窗</h2>
+      <div class="sun-demo-nav-row">
+        <sun-cell title="圆角弹窗" is-link bg-cffect @click="showRound=true" />
+      </div>
+    </div>
+
+    <!-- 基础展示 -->
+    <sun-popup v-model="showCenter" position="center">内容</sun-popup>
+    <sun-popup v-model="showTop" position="top" />
+    <sun-popup v-model="showBottom" position="bottom" />
+    <sun-popup v-model="showLeft" position="left" />
+    <sun-popup v-model="showRight" position="right" />
+
+    <!-- 图标 -->
+    <sun-popup v-model="showClose1" position="bottom" closeable />
+    <sun-popup v-model="showClose2" position="bottom" closeable close-icon="plus" />
+    <sun-popup v-model="showClose3" position="bottom" closeable close-icon-position="top-left" />
+
+    <!-- 圆角 -->
+    <sun-popup v-model="showRound" position="bottom" round />
+  </div>
 </template>
 
 <script>
-export default {}
+  export default {
+    data() {
+      return {
+        showTop: false,
+        showCenter: false,
+        showBottom: false,
+        showRight: false,
+        showLeft: false,
+        position: 'center',
+        showClose1: false,
+        showClose2: false,
+        showClose3: false,
+        showRound: false
+      }
+    },
+    methods: {
+      handleShow(position) {
+        switch (position) {
+          case 'center':
+            this.showCenter = true
+            break
+          case 'top':
+            this.showTop = true
+            break
+          case 'bottom':
+            this.showBottom = true
+            break
+          case 'left':
+            this.showLeft = true
+            break
+          case 'right':
+            this.showRight = true
+            break
+        }
+      }
+    }
+  }
 </script>
 
 <style lang="less">
+  .sun-demo-popup {
+    margin-top: 49px;
+    min-height: 96vh;
+    background-color: #f8f8f8;
+
+    .sun-demo-block {
+      padding: 0 16px;
+    }
+
+    .sun-demo-nav-title {
+      padding: 16px 3px;
+      margin: 0;
+    }
+
+    .sun-demo-nav-row {
+      background-color: #fff;
+      border-radius: 12px;
+      margin-bottom: 10px;
+    }
+  }
 </style>
--- src/views/Toast.vue
+++ src/views/Toast.vue
@@ -1,10 +1,62 @@
 <template>
-  <div class="sun-demo-toast"></div>
+  <div class="sun-demo-toast">
+    <sun-nav-bar left-arrow @left-click="$router.back()" title="Toast" />
+    <!-- 基础用法 -->
+    <div class="sun-demo-block">
+      <h2 class="sun-demo-nav-title">基础用法</h2>
+      <div class="sun-demo-nav-row">
+        <sun-cell title="文字提示" is-link bg-cffect @click="$toast('提示内容')" />
+        <sun-cell title="加载提示" is-link bg-cffect @click="$toast.loading('加载中...')" />
+        <sun-cell title="成功提示" is-link bg-cffect @click="$toast.success('成功提示')" />
+        <sun-cell title="失败提示" is-link bg-cffect @click="$toast.fail('失败提示')" />
+      </div>
+    </div>
+
+    <!-- 自定义图标 -->
+    <div class="sun-demo-block">
+      <h2 class="sun-demo-nav-title">自定义图标</h2>
+      <div class="sun-demo-nav-row">
+        <sun-cell title="自定义图标" is-link bg-cffect @click="$toast({message:'自定义图标',icon:'like-o'})" />
+        <sun-cell title="自定义图片" is-link bg-cffect @click="$toast.loading('加载中...')" />
+        <sun-cell title="自定义加载图标" is-link bg-cffect @click="$toast.success('成功提示')" />
+      </div>
+    </div>
+
+    <!-- 自定义位置 -->
+    <div class="sun-demo-block">
+      <h2 class="sun-demo-nav-title">自定义位置</h2>
+      <div class="sun-demo-nav-row">
+        <sun-cell title="顶部展示" is-link bg-cffect @click="$toast({message:'顶部展示', position: 'top'})" />
+        <sun-cell title="底部展示" is-link bg-cffect @click="$toast({message:'底部展示', position: 'bottom'})" />
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
-export default {}
+  export default {}
 </script>
 
 <style lang="less">
+  .sun-demo-toast {
+    margin-top: 49px;
+    min-height: 96vh;
+    background-color: #f8f8f8;
+
+    .sun-demo-block {
+      padding: 0 16px;
+    }
+
+    .sun-demo-nav-title {
+      padding: 16px 3px;
+      margin: 0;
+    }
+
+    .sun-demo-nav-row {
+      background-color: #fff;
+      border-radius: 12px;
+      margin-bottom: 10px;
+    }
+  }
 </style>
