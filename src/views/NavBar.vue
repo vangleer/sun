6d0536743d38bdf788a5b9b2fdab0b0d57313202
hong <1060850932@qq.com>
2020-09-30 17:31:52 +0800

    啦啦啦，放假了

--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -1,10 +1,72 @@
 <template>
-  <div class="sun-demo-nav-bar"></div>
+  <div class="sun-demo-nav-bar">
+    <sun-nav-bar title="NavBar" left-arrow />
+    <!-- 基础用法 -->
+    <div class="sun-demo-block">
+      <h2 class="sun-demo-nav-title">基础用法</h2>
+      <div class="sun-demo-nav-box">
+        <sun-nav-bar title="标题" left-text="返回" right-text="按钮" left-arrow @click-left="onClickLeft"
+          @click-right="onClickRight" />
+      </div>
+    </div>
+
+    <!-- 使用插槽 -->
+    <div class="sun-demo-block">
+      <h2 class="sun-demo-nav-title">使用插槽</h2>
+      <div class="sun-demo-nav-box">
+        <sun-nav-bar title="标题" left-text="返回" left-arrow>
+          <template #right>
+            <sun-icon name="plus" size="18" />
+          </template>
+        </sun-nav-bar>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+  export default {
+    components: {},
+    data() {
+      return {}
+    },
+    methods: {
+      onClickLeft() {
+        this.$toast('返回');
+      },
+      onClickRight() {
+        this.$Toast('按钮');
+      },
+    },
+    created() {}
+  }
 </script>
 
 <style lang="less">
+  .sun-demo-nav-bar {
+    padding-top: 49px;
+    width: 100vw;
+    height: 100vh;
+    background-color: #f8f8f8;
+
+    .sun-demo-nav-title {
+      padding: 16px 16px 12px;
+    }
+
+    .sun-demo-nav-box {
+      position: relative;
+      height: 30px;
+
+      .sun-nav-bar {
+        position: absolute;
+        background-color: #fff;
+        color: #1989fa;
+
+        .sun-nav-bar-left,
+        .sun-nav-bar-right {
+          font-size: 14px;
+        }
+      }
+    }
+  }
 </style>
