01058da2ab7346abd435079e0d930631cd254133
hong <1060850932@qq.com>
2020-10-10 09:39:46 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,10 @@ export default {}
 </script>
 
 <style>
+html::-webkit-scrollbar {
+  width: 0;
+  background-color: transparent;
+}
 .sun-demo-nav-title {
   margin: 24px 0 8px 0;
   color: rgba(69, 90, 100, 0.6);
