b52c45817c1609a3ec499bb8c4435a41dc7a1cb0
hong <1060850932@qq.com>
2020-11-12 17:12:46 +0800

    项目打包测试

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  publicPath: './'
+  publicPath: './',
+  productionSourceMap: false,
+  chainWebpack: config => {
+    if (process.env.NODE_ENV !== 'production') {
+      config
+        .plugin('uglifyjs-plugin')
+        .use('uglifyjs-webpack-plugin', [{
+          uglifyOptions: {
+            warnings: false,
+            compress: {
+              drop_console: true,
+              drop_debugger: false,
+              pure_funcs: ['console.log']
+            }
+          }
+        }])
+        .end()
+    }
+  }
 }
